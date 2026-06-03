63f218a5ff50511f79274758a484d4afecc6f7fa
Erik Antonio Alarcón Pinedo <C8VMM@ferrovial.com>
2026-06-02 22:07:39 -0500

    x

--- src/main/java/org/isobit/erp/MainVerticle.java
+++ src/main/java/org/isobit/erp/MainVerticle.java
@@ -1,18 +1,20 @@
 package org.isobit.erp;
 
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.VerticleBase;
 
-public class MainVerticle extends VerticleBase {
+public class MainVerticle extends AbstractVerticle {
 
   @Override
-  public Future<?> start() {
-    return vertx.createHttpServer().requestHandler(req -> {
-      req.response()
+  public Future<Void> start() {
+    int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+
+    return vertx.createHttpServer()
+      .requestHandler(req -> req.response()
         .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onSuccess(http -> {
-      System.out.println("HTTP server started on port 8888");
-    });
+        .end("Hello from Vert.x!"))
+      .listen(port, "0.0.0.0")
+      .onSuccess(server -> System.out.println("HTTP server started on port " + port))
+      .mapEmpty();
   }
 }
